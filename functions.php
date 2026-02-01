61682234ad5487962d958b6731bfb78eec68caa8
Erion-Pllana <erionpllana35@gmail.com>
2026-02-01 17:04:01 +0100

    .

--- functions.php
+++
@@ -1,9 +0,0 @@
-function rbp_enqueue_assets() {
-  wp_enqueue_style(
-    'rbp-main-style',
-    get_template_directory_uri() . '/assets/css/main.css',
-    [],
-    '1.0'
-  );
-}
-add_action('wp_enqueue_scripts', 'rbp_enqueue_assets');
